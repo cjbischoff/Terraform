aa0c22f62c6e769e6336e0cb374a39aacd638d4e
Christopher.Bischoff <cbischoff@practicefusion.com>
2018-02-16 10:53:44 -0700

    [feature/Create-terraform-remote-state-bucket] Refactor for long-term support

--- terraform-remote-state/ue1/kms/outputs_kms.tf
+++ terraform-remote-state/ue1/kms/outputs_kms.tf
@@ -1,3 +1,3 @@
-output "terraform_remote_state_kms_arn" {
-  value = "${aws_kms_key.terraform_remote_state.arn}"
+output "terraform_remote_state_kms_key_arn" {
+  value = "${aws_kms_key.terraform_remote_state_key.arn}"
 }
--- terraform-remote-state/ue1/s3/terraform.tfvars
+++ terraform-remote-state/ue1/s3/terraform.tfvars
@@ -1,4 +1,4 @@
+identifier = "sliplab"
 region               = "us-east-1"
 region_short = "ue1"
-identifier = "sliplab"
 system = "terraform-remote-state"
